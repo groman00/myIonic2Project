012426a3448a98da5095a8427279c9c375de1f83
Roman, Greg <greg.roman@teamaol.com>
2016-05-10 12:05:20 -0400

    form with simple validation

--- app/pages/contact/contact.js
+++ app/pages/contact/contact.js
@@ -1,13 +1,39 @@
-import {FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/common';
+import { FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl } from 'angular2/common';
 import {Page} from 'ionic-angular';
-
 
 @Page({
   templateUrl: 'build/pages/contact/contact.html',
-  providers: [FormBuilder]
+  directives: [FORM_DIRECTIVES]
 })
 export class ContactPage {
-  constructor() {
+  static get parameters() {
+    return [[FormBuilder]];
+  }
+  
+  constructor(fb) {
+
+    this.contactForm = ControlGroup;  
+
+    this.contactForm = fb.group({  
+      'name': ['', Validators.compose([Validators.required, Validators.minLength(2), this.checkFirstCharacterValidator])],
+      'email': ['', Validators.compose([Validators.required, this.checkFirstCharacterValidator])]
+    });
+ 
+    this.name = this.contactForm.controls['name'];
+    this.email = this.contactForm.controls['email'];
 
   }
+
+  onSubmit(value){ 
+      if(this.contactForm.valid) {
+          console.log('Submitted value: ', value);
+      }
+  }
+
+  checkFirstCharacterValidator(control){  
+      if (control.value.match(/^\d/)) {  
+          return {checkFirstCharacterValidator: true};  
+      }       
+  }
+
 }
